64a2f7ce9f939d763ae62c5609c04635912dbbab
Andrea <andiserm@gmail.com>
2023-02-15 20:53:30 -0300

    Agrego solucion

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  console.log(JSON.stringify(req.headers['user-agent']))
-  res.send(`<p>${JSON.stringify(req.headers)}</p>`)
+  res.send(`<p>${req.headers['user-agent']}</p>`)
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
